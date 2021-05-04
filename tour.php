ebe02d1c2fef17d2a26109b42ac1c9a5ff692f29
Josh Barron <60366943+joshuabarron1997@users.noreply.github.com>
2021-05-04 02:57:28 -0500

    complete

--- tour.php
+++ tour.php
@@ -142,6 +142,7 @@ function checkBoxes($value){
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	
 	<link rel="stylesheet" type="text/css" href="coopTemplateStyle.css">
+	<link rel = "manifest" href = "/manifest.json">
 	<style>	
 		#contact {
 			width: 80%;
